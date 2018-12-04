939b0e855258aeaabc0a6f2af205a766c43093c3
Ubuntu <ubuntu@ip-172-31-44-136.ec2.internal>
2018-12-04 20:36:36 +0000

    added tool_root.xml

--- HW40/root/compute_root.py
+++ HW40/root/compute_root.py
@@ -1,6 +1,7 @@
-import argparse
+import Rappture
+import sys
+from math import *
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from scipy import optimize
 
@@ -15,40 +16,49 @@ def main():
 
     # Get arguments
 
-    parser = argparse.ArgumentParser(description='Find root')
+    io = Rappture.library(sys.argv[1])
 
-    parser.add_argument('formula', type=str, help='f(x)')
+    xmin = float(io.get('input.number(min).current'))
+    xmax = float(io.get('input.number(max).current'))
+    formula = io.get('input.string(formula).current')
+    a = float(io.get('input.number(a).current'))
 
-    parser.add_argument('--xmin', type=float, default = 0, help='min x')
+    #parser = argparse.ArgumentParser(description='Find root')
 
-    parser.add_argument('--xmax', type=float, default = 100, help='max x')
+    #parser.add_argument('formula', type=str, help='f(x)')
 
-    parser.add_argument('--a', type=float, default = 10, help='a')
+    #integrating the function
 
-    args = parser.parse_args()
+
 
     # Get base string
 
-    my_str_base = 'int_0^root (' + args.formula + ') dx - ' + str(args.a)
+    my_str_base = 'int_0^root (' + str(formula) + ') dx - ' + str(a)
 
     # Get compute root of \int_0^x f(x') dx' - a
 
     root = optimize.brentq(
-               calc, args.xmin, args.xmax, args=(args.a, args.formula)
+               calc, xmin, xmax, args=(a,formula)
            )
 
-    my_str = '\nRoot of ' + my_str_base +  ' in [' + str(args.xmin) + \
-            ', ' + str(args.xmax) + '] is ' + str(root)
+    my_str = '\nRoot of ' + my_str_base +  ' in [' + str(xmin) + \
+            ', ' + str(xmax) + '] is ' + str(root)
 
-    print(my_str)
+    io.put('output.string(result1).about.label', 'Root')
+    io.put('output.string(result1).current', my_str)
+    #print(my_str)
 
     # Check
 
-    check = calc(root, args.a, args.formula)
+    check = calc(root, a, formula)
 
     my_str2 = '\nCheck: ' + my_str_base + ' = ' + str(check[0])
 
-    print(my_str2 + '\n')
+    io.put('output.string(result2).about.label', 'check')
+    io.put('output.string(result2).current', my_str2 )
+    #print(my_str2 + '\n')
+
+    Rappture.result(io)
 
 if __name__ == "__main__":
     main()
